5fd36abd6b2e9b7c17d05ee99c0d6d714ebfd369
yaping.pan <pan.yaping@le-yao.com>
2019-11-12 14:48:05 +0800

    (分类)右侧scroll-view滚动监测选中左侧分类

--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -7,16 +7,15 @@ Page({
   data: {
     categoryData: null,
     currentCategoryIndex: null,
-    productList: []
+    intoViewId: 'rightSide_id'
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       categoryData: getApp().globalData.categoryItem,
-      productList: getApp().globalData.categoryItem[0]['products'],
       currentCategoryIndex: 0
     })
   },
@@ -24,58 +23,101 @@ Page({
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  changecategory: function (event) {
+  changecategory: function(event) {
     let index = event.currentTarget.dataset.index;
-    console.log(this.data.categoryData[index])
+    console.log('#category_' + index, )
+    //获取
     this.setData({
-      productList: this.data.categoryData[index]['products'],
-      currentCategoryIndex: index
+      currentCategoryIndex: index,
+      intoViewId: 'category_' + index
     })
+  },
+  // 监听滑动，控制左侧栏选中状态
+  bindscroll: function(event) {
+    const query = wx.createSelectorQuery();
+    let containViewBottom;
+    let categoryItem;
+    let categoryLength = this.data.categoryData.length;
+    if (categoryLength === 0 || categoryLength === 1) {
+      return;
+    }
+    async function f(that) {
+      let promise1 = new Promise((resolve, reject) => {
+        query.select('#rightSide_id').boundingClientRect().exec(res => {
+          resolve(res[0].bottom)
+        })
+      })
+      containViewBottom = await promise1;
+      let promise2 = new Promise((resolve, reject) => {
+        let item = { index: undefined, isbreak: false };
+        for (let i = 0; i < categoryLength; i++) {
+          query.select('#category_' + i).boundingClientRect().exec(res => {
+            if (!item.isbreak && res[i + 1].top < 50) {
+              item = { index: i, isbreak: false };
+            }
+            if (res[i + 1].top < containViewBottom && res[i + 1].top > 50) {
+              item = { index: i, isbreak: true };
+            }
+            if(item.isbreak || i === categoryLength -1) {
+              resolve(item)
+            }
+          })
+        }
+      })
+      categoryItem = await promise2;
+      let index = categoryItem.index;
+      if (index !== undefined && index !== that.data.currentCategoryIndex) {
+        that.setData({
+          currentCategoryIndex: index,
+        })
+      }
+    }
+    f(this);
   }
 })
